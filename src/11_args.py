4d0f19e4909596946fb68772c03714427bc02074
[Brandon] <desselle.brandon2@gmail.com>
2019-12-03 19:19:31 -0600

    Update 11_args.py

--- src/11_args.py
+++ src/11_args.py
@@ -4,14 +4,20 @@
 # Write a function f1 that takes two integer positional arguments and returns
 # the sum. This is what you'd consider to be a regular, normal function.
 
-# YOUR CODE HERE
+
+def f1(i_1: int, i_2: int) -> int:
+    return i_1 + i_2
+
 
 print(f1(1, 2))
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
-# YOUR CODE HERE
+
+def f2(*args):
+    return sum(args)
+
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +27,19 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print(f2(*a))    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
-# YOUR CODE HERE
+
+def f3(one=None, two=None):
+    if one and two:
+        return one + two
+    else:
+        return one + 1
+
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -40,8 +52,10 @@ print(f3(8))     # Should print 9
 # key: baz, value: 12
 #
 # Google "python keyword arguments".
+def f4(**kwargs):
+    for k, v in kwargs.items():
+        print(f'key: {k}, value: {v}')
 
-# YOUR CODE HERE
 
 # Should print
 # key: a, value: 12
@@ -60,4 +74,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
