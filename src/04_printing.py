e3b3b004de2b8a4bfbb261ee54e5d83bf7cc1fdf
[Brandon] <desselle.brandon2@gmail.com>
2019-12-03 18:37:49 -0600

    Update 04_printing.py

--- src/04_printing.py
+++ src/04_printing.py
@@ -11,14 +11,26 @@ z = "I like turtles!"
 # Using the printf operator (%), print the following feeding in the values of x,
 # y, and z:
 # x is 10, y is 2.25, z is "I like turtles!"
+print('x is %d, y is %.2f, z is "%s"' % (x, y, z))
 
-print('x is %d, y is %f, z is "%s"' % (x, y, z))
+"""
+Printf-style String Formatting
+We use the modulus operator (%) to signify the start of a specifier followed by a character that determines the formatting that will be applied
+
+#####Conversions#####
+%d  |  Signed integer decimal
+%f  |  Floating point decimal format
+%s  |  String (converts any python object using str())
+#####Conversions#####
+"""
 
 # Use the 'format' string method to print the same thing
-
-str = 'x is {}, y is {}, z is "{}"'
+str = 'x is {:d}, y is {:.2f}, z is "{:s}"'
 print(str.format(x, y, z))
 
-# Finally, print the same thing using an f-string
+"""
+"format" works almost identically to the "printf" method of formatting. Instead of using the modulus operator we use the the ":" character to signify the start of a specifier
+"""
 
-f'x is {x}, y is {y}, z is {z}.'
+# Finally, print the same thing using an f-string
+print(f'x is {x}, y is {y:.2f}, z is "{z}"')
