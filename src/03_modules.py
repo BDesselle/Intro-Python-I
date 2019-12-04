84c0534600283856ba9597d80bf44fcae95324ff
[Brandon] <desselle.brandon2@gmail.com>
2019-12-03 18:37:47 -0600

    Update 03_modules.py

--- src/03_modules.py
+++ src/03_modules.py
@@ -6,27 +6,49 @@ level operating system functionality.
 """
 
 import os
+# The "os" module provides a portable way of using operating system dependent functionality.
 import sys
-# See docs for the sys module: https://docs.python.org/3.7/library/sys.html
+# The "sys" module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter.
 
 # Print out the command line arguments in sys.argv, one per line:
 for args in sys.argv:
     print(args)
 
+"""
+"argv" returns a list of command line args passed to a python script.
+"""
+
 # Print out the OS platform you're using:
 print(sys.platform)
+
+"""
+"platform" returns a string containing the platform value of your OS.
+"""
 
 # Print out the version of Python you're using:
 print(sys.version)
 
-
-# See the docs for the OS module: https://docs.python.org/3.7/library/os.html
+"""
+"version" returns a string containing the version number of your current python interpreter.
+"""
 
 # Print the current process ID
 print(os.getpid())
 
+"""
+"getpid" returns the current process id.
+"""
+
 # Print the current working directory (cwd):
 print(os.getcwd())
 
+"""
+"getcwd" returns a string representing the current working directory.
+"""
+
 # Print out your machine's login name
-print(os.getenv('COMPUTERNAME'))
+print("Machine: " + os.getenv('COMPUTERNAME'), "\nUser: " + os.getenv('USERNAME') )
+
+"""
+"getenv" returns a string containing the value of the environment variable key if it exists.
+"""
